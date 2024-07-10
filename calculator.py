544209871678d09993f06d54bf3753c871759a34
maahi3793 <maahi.amdocs@gmail.com>
2024-07-10 23:11:41 +0200

    end of day 3
    which is basically day 1 of python course

--- calculator.py
+++ calculator.py
@@ -1,2 +1,27 @@
 # Let's code a calculator
 
+x = int(input("enter a number: "))
+y = int(input("Enter another number: "))
+
+z = x + y
+
+print(z)
+print(f"{z:,}")
+
+
+a = float(input("enter a fraction number: "))
+b = float(input("enter another fraction number: "))
+
+c = a + b
+
+print(c)
+print(round(c))
+
+
+num1 = float(input("enter any number: "))
+num2 = float(input("enter any number again: "))
+
+div = num1 / num2
+
+print(div)
+print(f"{div:,.2f}")
